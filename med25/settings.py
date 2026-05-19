9dc0630705a327326caeac8ce0934f89d0353d3a
VuDucZuy <duczuyvu12@gmail.com>
2026-05-19 21:45:05 +0300

    Fix automatic redirect from HTTP to HTTPS

--- med25/settings.py
+++ med25/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-n-0c@@kj91f9j-^w$s0@j2r&0t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -82,6 +82,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'med25.wsgi.application'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = False
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
 
 
 # Database
